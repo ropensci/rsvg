273a40c05a981d7a3ba397b460f205febb9f9504
Jeroen Ooms <jeroenooms@gmail.com>
2016-01-05 02:40:11 +0100

    Update docs

--- R/rsvg.R
+++ R/rsvg.R
@@ -1,15 +1,16 @@
 #' Render SVG into Bitmap
 #'
-#' Render svg data into high quality bitmap. When neither \code{width} or \code{height}
-#' is specified, the output resolution matches that of the input. When either \code{width}
+#' Render svg image into a high quality bitmap. When both \code{width} and \code{height}
+#' are \code{NULL}, the output resolution matches that of the input. When either \code{width}
 #' or \code{height} is specified, the image is scaled proportionally. When both \code{width}
 #' and \code{height} are specified, the image is stretched into the requested size.
 #'
 #' @export
 #' @useDynLib rsvg R_rsvg
-#' @param path to svg file or raw vector with svg data
-#' @parm width output width in pixes. If NULL the input size will be used.
-#' @parm height output height in pixes. If NULL the input size will be used.
+#' @param svg path to svg file or raw vector with svg data. Use \link{charToRaw} to convert
+#' strings into raw vectors.
+#' @param width output width in pixels or \code{NULL} for default.
+#' @param height output height in pixels or \code{NULL} for default
 rsvg <- function(svg, width = NULL, height = NULL) {
   if(is.character(svg)){
     if(file.exists(svg)){
@@ -19,6 +20,8 @@ rsvg <- function(svg, width = NULL, height = NULL) {
     }
   }
   stopifnot(is.raw(svg))
+  stopifnot(is.null(width) || is.numeric(width))
+  stopifnot(is.null(height) || is.numeric(height))
   out <- .Call(R_rsvg, svg, width, height)
   out <- structure(as.numeric(out)/255, dim = dim(out))
   aperm(out)[,,c(3,2,1,4)] # Convert to standard with*height*rgba
