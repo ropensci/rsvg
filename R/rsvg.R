2315a44056b0443151b3660367f24305ba4ac5ed
Jeroen Ooms <jeroenooms@gmail.com>
2016-01-05 02:53:45 +0100

    Tweaks

--- R/rsvg.R
+++ R/rsvg.R
@@ -11,6 +11,17 @@
 #' strings into raw vectors.
 #' @param width output width in pixels or \code{NULL} for default.
 #' @param height output height in pixels or \code{NULL} for default
+#' @examples # create some svg
+#' tmp <- tempfile()
+#' svglite::svglite(tmp, width = 10, height = 7)
+#' ggplot2::qplot(mpg, wt, data = mtcars, colour = factor(cyl))
+#' dev.off()
+#'
+#' # render it into 720p HD
+#' bitmap <- rsvg(tmp, height = 720)
+#' png::writePNG(bitmap, "test.png", dpi = 144)
+#' jpeg::writeJPEG(bitmap, "test.jpg", quality = 1)
+#' webp::write_webp(bitmap, "test.webp", quality = 100)
 rsvg <- function(svg, width = NULL, height = NULL) {
   if(is.character(svg)){
     if(file.exists(svg)){
