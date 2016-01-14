8b1a9c19caab483be0ab79547ad43c1ef08c65d1
Jeroen Ooms <jeroenooms@gmail.com>
2016-01-15 00:01:37 +0100

    Bla

--- src/init.c
+++ src/init.c
@@ -2,7 +2,9 @@
 #include <librsvg/rsvg.h>
 
 void R_init_rsvg(DllInfo *info) {
+  #if defined(LIBRSVG_MAJOR_VERSION) && LIBRSVG_MAJOR_VERSION == 2
   #if defined(LIBRSVG_MINOR_VERSION) && LIBRSVG_MINOR_VERSION < 36
   g_type_init();
+  #endif
   #endif
 }
