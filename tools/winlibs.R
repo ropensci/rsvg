e3ef3dc41e87ee43f172e49dd6d946e70845c9f2
Jeroen Ooms <jeroenooms@gmail.com>
2016-08-03 19:58:37 +0200

    Upgrade windows build to 2.40.16

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,7 @@
-# Build against mingw-w64 build of libsodium 1.0.3
-if(!file.exists("../windows/rsvg-2.40.12/include/librsvg-2.0/librsvg/rsvg.h")){
+# Build against mingw-w64 build of librsvg 2.40.16
+if(!file.exists("../windows/rsvg-2.40.16/include/librsvg-2.0/librsvg/rsvg.h")){
   if(getRversion() < "3.3.0") setInternet2()
-  download.file("https://github.com/rwinlib/rsvg/archive/v2.40.12.zip", "lib.zip", quiet = TRUE)
+  download.file("https://github.com/rwinlib/rsvg/archive/v2.40.16.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
