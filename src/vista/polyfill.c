c8fce8aee197235cba308d2f82e1ef5bf6372182
Jeroen <jeroenooms@gmail.com>
2020-05-11 10:14:22 +0200

    Try this then

--- src/vista/polyfill.c
+++ src/vista/polyfill.c
@@ -1,8 +1,6 @@
 #include <Windows.h>
 
-#ifndef _WIN32_WINNT_WIN7
 WINBASEAPI WINBOOL WINAPI SetThreadErrorMode(DWORD dwNewMode, LPDWORD lpOldMode){
   SetErrorMode(dwNewMode);
   return TRUE;
 }
-#endif
