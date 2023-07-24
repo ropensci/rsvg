bd1c0b0e7351d65d607a7dd01cb5f5c7025fb3c9
Jeroen Ooms <jeroenooms@gmail.com>
2023-07-24 23:59:20 +0200

    Windows: update librsvg (#39)

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -2,7 +2,7 @@
 VERSION <- commandArgs(TRUE)
 if(!file.exists(sprintf("../windows/rsvg-%s/include/librsvg-2.0/librsvg/rsvg.h", VERSION))){
   if(getRversion() < "3.3.0") setInternet2()
-  download.file(sprintf("https://github.com/rwinlib/rsvg/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
+  download.file(sprintf("https://github.com/rwinlib/rsvg/archive/%s.zip", VERSION), "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
