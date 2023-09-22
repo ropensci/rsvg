0932353ca52acef54351abd8339deee58a17a85a
Jeroen Ooms <jeroenooms@gmail.com>
2023-09-22 13:50:34 +0200

    Windows: update librsvg

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,9 +1,18 @@
-# Build against static libraries from rwinlib
-VERSION <- commandArgs(TRUE)
-if(!file.exists(sprintf("../windows/rsvg-%s/include/librsvg-2.0/librsvg/rsvg.h", VERSION))){
-  if(getRversion() < "3.3.0") setInternet2()
-  download.file(sprintf("https://github.com/rwinlib/rsvg/archive/%s.zip", VERSION), "lib.zip", quiet = TRUE)
+
+
+if(!file.exists("../windows/librsvg/include/librsvg-2.0/librsvg/rsvg.h")){
+  unlink("../windows", recursive = TRUE)
+  url <- if(grepl("aarch", R.version$platform)){
+    "https://github.com/r-windows/bundles/releases/download/librsvg-2.50.9/librsvg-2.50.9-clang-aarch64.tar.xz"
+  } else if(getRversion() >= "4.2") {
+    "https://github.com/r-windows/bundles/releases/download/librsvg-2.50.9/librsvg-2.50.9-ucrt-x86_64.tar.xz"
+  } else {
+    "https://github.com/rwinlib/rsvg/archive/refs/tags/2.50.5-2.tar.gz"
+  }
+  download.file(url, basename(url), quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
-  unzip("lib.zip", exdir = "../windows")
-  unlink("lib.zip")
+  untar(basename(url), exdir = "../windows", tar = 'internal')
+  unlink(basename(url))
+  setwd("../windows")
+  file.rename(list.files(), 'librsvg')
 }
