f3ca19ccd44008b3669cf5717685160a9be50ca2
Jeroen Ooms <jeroenooms@gmail.com>
2023-09-24 22:14:32 +0200

    Linking to ntdll on msvcrt is dangerous

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -7,7 +7,7 @@ if(!file.exists("../windows/librsvg/include/librsvg-2.0/librsvg/rsvg.h")){
   } else if(getRversion() >= "4.2") {
     "https://github.com/r-windows/bundles/releases/download/librsvg-2.50.9/librsvg-2.50.9-ucrt-x86_64.tar.xz"
   } else {
-    "https://github.com/rwinlib/rsvg/archive/refs/tags/2.50.5-2.tar.gz"
+    "https://github.com/rwinlib/rsvg/archive/v2.50.5.tar.gz"
   }
   download.file(url, basename(url), quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
