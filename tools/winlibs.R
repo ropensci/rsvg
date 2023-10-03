069218b09454db438df7dd43a1de9e5dde215758
Jeroen Ooms <jeroenooms@gmail.com>
2023-10-03 15:32:57 +0200

    Windows: update to librsvg 2.57.0

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -3,9 +3,11 @@
 if(!file.exists("../windows/librsvg/include/librsvg-2.0/librsvg/rsvg.h")){
   unlink("../windows", recursive = TRUE)
   url <- if(grepl("aarch", R.version$platform)){
-    "https://github.com/r-windows/bundles/releases/download/librsvg-2.50.9/librsvg-2.50.9-clang-aarch64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/librsvg-2.57.0/librsvg-2.57.0-clang-aarch64.tar.xz"
+  } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
+    "https://github.com/r-windows/bundles/releases/download/librsvg-2.57.0/librsvg-2.57.0-clang-x86_64.tar.xz"
   } else if(getRversion() >= "4.2") {
-    "https://github.com/r-windows/bundles/releases/download/librsvg-2.50.9/librsvg-2.50.9-ucrt-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/librsvg-2.57.0/librsvg-2.57.0-ucrt-x86_64.tar.xz"
   } else {
     "https://github.com/rwinlib/rsvg/archive/v2.50.5.tar.gz"
   }
