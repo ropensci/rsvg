3e981896e2ce6f57c8d3925dc6bebaf712ce86a2
Jeroen Ooms <jeroenooms@gmail.com>
2023-10-05 21:44:14 +0200

    Test rsvg_svg()

--- tests/testthat/test-render-size.R
+++ tests/testthat/test-render-size.R
@@ -5,23 +5,44 @@ image_dim <- function(path){
   sprintf('%dx%d', info$width, info$height)
 }
 
+image_similarity <- function(path1, path2){
+  im1 <- magick::image_read(path1)
+  im2 <- magick::image_read(path2)
+  magick::image_compare_dist(im1, im2)$distortion
+}
+
 test_that("rendering to size works", {
   skip_on_cran()
-  tmp <- tempfile(fileext = '.png')
+  tmp1 <- tempfile(fileext = '.png')
+  tmp2 <- tempfile(fileext = '.png')
+  orig <- 'au.svg'
+  normalized <- rsvg_svg(orig)
 
   # Use natural size
-  rsvg_png('au.svg', tmp)
-  expect_equal(image_dim(tmp), '512x512')
+  rsvg_png(orig, tmp1)
+  rsvg_png(normalized, tmp2)
+  expect_equal(image_dim(tmp1), '512x512')
+  expect_equal(image_dim(tmp2), '512x512')
+  expect_gt(image_similarity(tmp1, tmp2), 0.99)
 
   # Scale by width
-  rsvg_png('au.svg', tmp, width = 256)
-  expect_equal(image_dim(tmp), '256x256')
+  rsvg_png(orig, tmp1, width = 256)
+  rsvg_png(normalized, tmp2, width = 256)
+  expect_equal(image_dim(tmp1), '256x256')
+  expect_equal(image_dim(tmp2), '256x256')
+  expect_gt(image_similarity(tmp1, tmp2), 0.99)
 
   # Scale by height
-  rsvg_png('au.svg', tmp, height = 1024)
-  expect_equal(image_dim(tmp), '1024x1024')
+  rsvg_png(orig, tmp1, width = 1024)
+  rsvg_png(normalized, tmp2, width = 1024)
+  expect_equal(image_dim(tmp1), '1024x1024')
+  expect_equal(image_dim(tmp2), '1024x1024')
+  expect_gt(image_similarity(tmp1, tmp2), 0.99)
 
   # Force stretch
-  rsvg_png('au.svg', tmp, width = 512, height = 1024)
-  expect_equal(image_dim(tmp), '512x1024')
+  rsvg_png(orig, tmp1, width = 512, height = 1024)
+  rsvg_png(normalized, tmp2, width = 512, height = 1024)
+  expect_equal(image_dim(tmp1), '512x1024')
+  expect_equal(image_dim(tmp2), '512x1024')
+  expect_gt(image_similarity(tmp1, tmp2), 0.99)
 })
