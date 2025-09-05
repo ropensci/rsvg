04de65ea4df7f5a464db0b696a5a1f9d55731385
Jeroen Ooms <jeroenooms@gmail.com>
2025-09-06 01:24:30 +0200

    Simplify more

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -4,10 +4,8 @@ if(!file.exists('rsvg.o') && !file.exists("../.deps/librsvg/include/librsvg-2.0/
     "https://github.com/r-windows/bundles/releases/download/librsvg-2.61.0/librsvg-2.61.0-clang-aarch64.tar.xz"
   } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
     "https://github.com/r-windows/bundles/releases/download/librsvg-2.61.0/librsvg-2.61.0-clang-x86_64.tar.xz"
-  } else if(getRversion() >= "4.3") {
-    "https://github.com/r-windows/bundles/releases/download/librsvg-2.61.0/librsvg-2.61.0-ucrt-x86_64.tar.xz"
   } else if(getRversion() >= "4.2") {
-    "https://github.com/r-windows/bundles/releases/download/librsvg-2.57.0/librsvg-2.57.0-ucrt-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/librsvg-2.61.0/librsvg-2.61.0-ucrt-x86_64.tar.xz"
   } else {
     "https://github.com/rwinlib/rsvg/archive/v2.50.5.tar.gz"
   }
